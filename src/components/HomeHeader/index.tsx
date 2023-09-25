807b1da74c17407e82c483574a6bb7a170ddf774
GabrielDsanta <eloviveiros@hotmail.com>
2023-09-25 13:58:05 -0300

    feat: Criando componente Header, Instalando Realm no projeto e adicionardo o AppProvider, Finalizando Função de Login

---
+++ src/components/HomeHeader/index.tsx
@@ -0,0 +1,25 @@
+import { TouchableOpacity } from "react-native";
+import { Container, Greeting, Message, Name } from "./styles";
+import { Power } from "phosphor-react-native"
+import theme from "../../theme";
+
+
+export function HomeHeader(){
+    return(
+        <Container>
+            <Greeting>
+                <Message>
+                    Olá
+                </Message>
+
+                <Name>
+                    Gabriel
+                </Name>
+            </Greeting>
+
+            <TouchableOpacity>
+                <Power size={32} color={theme.COLORS.GRAY_400} />
+            </TouchableOpacity>
+        </Container>
+    )
+}
--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,11 +1,56 @@
-import { useState } from "react"
+import { useEffect, useState } from "react";
 import { Container, Title, Slogan } from "./styles";
 import { Button } from "../../components/Button";
+import { ANDROID_CLIENT_ID, IOS_CLIENT_ID } from "@env";
+import { Alert } from "react-native";
+import { Realm, useApp } from "@realm/react";
 
-import backgroundImageSignIn from "../../assets/background.png"
+import backgroundImageSignIn from "../../assets/background.png";
+
+import * as WebBrowser from "expo-web-browser";
+import * as Google from "expo-auth-session/providers/google";
+
+WebBrowser.maybeCompleteAuthSession();
 
 export function SignIn() {
-  const [isLoading, setIsLoading] = useState(false)
+  const [isLoading, setIsLoading] = useState(false);
+
+  const app = useApp()
+
+  const [_, response, googleSignIn] = Google.useAuthRequest({
+    androidClientId: ANDROID_CLIENT_ID,
+    iosClientId: IOS_CLIENT_ID,
+    scopes: ["profile", "email"],
+  });
+
+  function handleGoogleSignIn() {
+    setIsLoading(true);
+
+    googleSignIn().then((response) => {
+      if (response.type !== "success") {
+        setIsLoading(false);
+      }
+    });
+  }
+
+  useEffect(() => {
+    if (response?.type === "success") {
+      if (response.authentication?.idToken) {
+        const credencials = Realm.Credentials.jwt(
+          response.authentication?.idToken
+        );
+
+        app.logIn(credencials).catch((error) => {
+          console.log(error)
+          Alert.alert("Não foi possível conectar-se a sua conta Google");
+          setIsLoading(false)
+        })
+      } else {
+        Alert.alert("Não foi possível conectar-se a sua conta Google");
+        setIsLoading(false)
+      }
+    }
+  }, [response]);
 
   return (
     <Container source={backgroundImageSignIn}>
@@ -13,7 +58,11 @@ export function SignIn() {
 
       <Slogan>Gestão de uso de veículos</Slogan>
 
-      <Button isLoading={isLoading} title="Entrar com Google"/>
+      <Button
+        onPress={handleGoogleSignIn}
+        isLoading={isLoading}
+        title="Entrar com Google"
+      />
     </Container>
   );
 }
